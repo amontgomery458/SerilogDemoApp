3dad0ff87c2ad86c313d4b47b597649acefe757e
amontgomery458 <145405580+amontgomery458@users.noreply.github.com>
2024-04-01 14:29:31 -0700

    Setting up Serilog

--- SerilogDemo/Pages/Index.cshtml.cs
+++ SerilogDemo/Pages/Index.cshtml.cs
@@ -14,7 +14,26 @@ namespace SerilogDemo.Pages
 
 		public void OnGet()
 		{
+			_logger.LogInformation("You requested the index page.");
 
+			try
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					if ( i == 56)
+					{
+						throw new Exception("This is our demo exception");
+					}
+					else
+					{
+						_logger.LogInformation("The value of i is {LoopCountValue}", i);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "We caught this exception in the Index Get call.");
+			}
 		}
 	}
 }
--- SerilogDemo/Program.cs
+++ SerilogDemo/Program.cs
@@ -1,7 +1,21 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+//var configuration = new ConfigurationBuilder()
+//	.AddJsonFile("appsettings.json")
+//	.Build();
+
+//Log.Logger = new LoggerConfiguration()
+//	.ReadFrom.Configuration(configuration)
+//	.CreateLogger();
+
+//Add support to logging with SERILOG
+builder.Host.UseSerilog((context, configuration) =>
+	configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
@@ -10,12 +24,13 @@ if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
+	//app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseSerilogRequestLogging();
 app.UseRouting();
 
 app.UseAuthorization();
